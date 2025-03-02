13e37e63545bece4c4f8196b06b4d30440946ae2
Janio Amorim <janioamorim04@gmail.com>
2025-03-02 11:42:00 -0300

    feat: rota para listar pedidos

--- src/app/controllers/DashboardsController.js
+++ src/app/controllers/DashboardsController.js
@@ -1,6 +1,19 @@
 import Order from "../models/Order";
 
 class DashboardsController {
-    
+    async index(req, res) {
+        try {
+          const orders = await Order.find();
+          return res.status(200).json({
+            success: true,
+            data: orders
+          });
+        } catch (error) {
+          return res.status(500).json({
+            error: true,
+            message: 'Erro ao buscar os dados do dashboard.'
+          });
+        }
+      }
 }
 export default new DashboardsController();
